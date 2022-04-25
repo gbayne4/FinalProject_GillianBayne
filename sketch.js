59f0fda2263b9a86ac494c1385162404ebb16dba
gillian <98335596+gbayne4@users.noreply.github.com>
2022-04-25 11:37:00 -0400

    basic scene
    Got the basic scene set up, I'm currently struggling with the collisions as the player move to the opposite end upon collisions instead of it being a barrier.

--- sketch.js
+++ sketch.js
@@ -1,28 +1,43 @@
 
-var littleMan;
-var trees;
+var littleMan; //main character
+var trees; //background 
 let moveX, moveY;
+var SCENE_W = 3000; //scene barrier width
+var SCENE_H = 3000; //scene barrier height
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	moveX = width/2;
 	moveY = height/2;
-	background(100);
 	trees = new Group();
-}
+	//create player
+	littleMan = createSprite(width/2, height/2, 40, 40);
+	
+	
+	
+	
+	//generate trees in background
+	for(var i = 0; i<300; i++) {
+  var newTrees = createSprite(random(-width, SCENE_W+width), random(-height, SCENE_H+height), random(50,80),random(80,130));
+	newTrees.setCollider('rectangle',  0, 0, 40,40); //makes trees collidable
+//	newTrees.immovable = true;
+	trees.add(newTrees);
+}}
 
 function draw() {
-	littleMan = createSprite(moveX, moveY, 50, 100);
+	background(15,30,127);
+	//set littleMans motion
+	littleMan.position.x = moveX
+	littleMan.position.y = moveY
 	
-	littleMan.velocity.x = (camera.mouseX-littleMan.position.x)/20;
-  littleMan.velocity.y = (camera.mouseY-littleMan.position.y)/20;
 	
+	//controlling the little main with keyboard
 	if (keyIsDown(LEFT_ARROW)) {
-    moveX += 2;
+    moveX -= 2; 
   }
 
   if (keyIsDown(RIGHT_ARROW)) {
-    moveX -= 2;
+    moveX  += 2;
   }
 
   if (keyIsDown(UP_ARROW)) {
@@ -32,8 +47,24 @@ function draw() {
   if (keyIsDown(DOWN_ARROW)) {
     moveY += 2;
   }
-
 	
+	//littleMan bounces off trees when he collides
+	trees.displace(littleMan, false);
+
+	//keeps littleMan within the game
+	if(littleMan.position.x < -width/2)
+    littleMan.position.x = -width/2;
+  if(littleMan.position.y < -height/2)
+    littleMan.position.y = -height/2;
+  if(littleMan.position.x > SCENE_W)
+    littleMan.position.x = SCENE_W;
+  if(littleMan.position.y > SCENE_H)
+    littleMan.position.y = SCENE_H;
+	
+		//camera following the player	
 	camera.position.x = littleMan.position.x;
 	camera.position.y = littleMan.position.y;
+	
+	
+drawSprites();
 }
