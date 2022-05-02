15276d3e72184787b137da3bdbb711a4ef00b1a7
gillian <98335596+gbayne4@users.noreply.github.com>
2022-05-01 21:00:55 -0400

    Figured out my problem!!
    I was having some difficulty with figuring out how to get it to overlap without glitching. I first stripped it down and saw the problem was with the key movements and positioning. I ended up having the to have the velocity change rather than directly having the position change.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
+//I figured it out!!
 
 var littleMan; //main character
-var trees; //background 
+var tTrees
+var sTrees; //background 
 let moveX, moveY;
 var SCENE_W = 3000; //scene barrier width
 var SCENE_H = 3000; //scene barrier height
@@ -9,48 +11,55 @@ function setup() {
 	createCanvas(windowWidth, windowHeight);
 	moveX = width/2;
 	moveY = height/2;
-	trees = new Group();
+	sTrees = new Group();
+	tTrees = new Group();
 	//create player
 	littleMan = createSprite(width/2, height/2, 40, 40);
+	littleMan.setCollider('circle', -10, 2, 20)
+
 	
+//generate trees in background
+for(var i = 0; i<500; i++) {
+var shortTrees = createSprite(random(-width, SCENE_W+width), random(-height, SCENE_H+height), 40,70);
+	shortTrees.setCollider('rectangle',  0, 0, 30,60); //makes trees collidable
+	sTrees.add(shortTrees); 
+	sTrees.immovable = true;
 	
-	
-	
-	//generate trees in background
-	for(var i = 0; i<300; i++) {
-  var newTrees = createSprite(random(-width, SCENE_W+width), random(-height, SCENE_H+height), random(50,80),random(80,130));
-	newTrees.setCollider('rectangle',  0, 0, 40,40); //makes trees collidable
-//	newTrees.immovable = true;
-	trees.add(newTrees);
+var tallTrees = createSprite(random(-width, SCENE_W+width), random(-height, SCENE_H+height), 40,110);
+	tallTrees.setCollider('rectangle',  0, 0, 30,110); //makes trees collidable
+	tTrees.immovable = true;
+	tTrees.add(tallTrees); 
 }}
 
 function draw() {
 	background(15,30,127);
 	//set littleMans motion
-	littleMan.position.x = moveX
-	littleMan.position.y = moveY
+  littleMan.velocity.x = (moveX-littleMan.position.x)/10;
+  littleMan.velocity.y = (moveY-littleMan.position.y)/10;
+
 	
-	
+
 	//controlling the little main with keyboard
-	if (keyIsDown(LEFT_ARROW)) {
+	if (keyDown(LEFT_ARROW)) {
     moveX -= 2; 
   }
 
-  if (keyIsDown(RIGHT_ARROW)) {
+  if (keyDown(RIGHT_ARROW)) {
     moveX  += 2;
   }
 
-  if (keyIsDown(UP_ARROW)) {
+  if (keyDown(UP_ARROW)) {
     moveY -= 2;
   }
 
-  if (keyIsDown(DOWN_ARROW)) {
+  if (keyDown(DOWN_ARROW)) {
     moveY += 2;
   }
-	
-	//littleMan bounces off trees when he collides
-	trees.displace(littleMan, false);
 
+	//littleMan bounces off trees when he collides
+	littleMan.collide(sTrees);
+	littleMan.collide(tTrees);
+	
 	//keeps littleMan within the game
 	if(littleMan.position.x < -width/2)
     littleMan.position.x = -width/2;
@@ -61,10 +70,12 @@ function draw() {
   if(littleMan.position.y > SCENE_H)
     littleMan.position.y = SCENE_H;
 	
-		//camera following the player	
+//camera following the player	
 	camera.position.x = littleMan.position.x;
 	camera.position.y = littleMan.position.y;
-	
-	
+
+tTrees.debug = mouseIsPressed;
+sTrees.debug = mouseIsPressed;
+littleMan.debug = mouseIsPressed;
 drawSprites();
 }
